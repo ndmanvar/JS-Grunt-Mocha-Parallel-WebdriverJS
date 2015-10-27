63d8d110d5e2efb422cdfd02ec38d11255d7273d
Neil <neil@Neils-MacBook-Pro.local>
2015-10-26 23:19:23 -0700

    Got the parallelization working

--- test/sample1-spec.js
+++ test/sample1-spec.js
@@ -1,54 +1,10 @@
 var assert = require('assert'),
-    test = require('selenium-webdriver/testing'),
     webdriver = require('selenium-webdriver'),
-    SauceLabs = require("saucelabs"),
-    username = process.env.SAUCE_USERNAME,
-    accessKey = process.env.SAUCE_ACCESS_KEY,
-    saucelabs = new SauceLabs({
-      username: username,
-      password: accessKey
-    });
+    makeSuite = require('./helpers').makeSuite;
 
-test.describe('Google Search 2', function() {
-  this.timeout(60000);
+makeSuite('Google Search 2', function() {
 
-  var driver;
-
-  test.beforeEach(function() {
-    var browser = process.env.BROWSER,
-        version = process.env.VERSION,
-        platform = process.env.PLATFORM,
-        server = "http://" + username + ":" + accessKey + 
-                  "@ondemand.saucelabs.com:80/wd/hub"; 
-
-    driver = new webdriver.Builder().
-      withCapabilities({
-        'browserName': browser,
-        'platform': platform,
-        'version': version,
-        'username': username,
-        'accessKey': accessKey,
-        'name': this.currentTest.title
-      }).
-      usingServer(server).
-      build();
-
-    driver.getSession().then(function (sessionid){
-      driver.sessionID = sessionid.id_;
-    });
-  });
-
-  test.afterEach(function(done) {
-    var passed = (this.currentTest.state === 'passed') ? true : false;
-
-    driver.quit();
-    
-    saucelabs.updateJob(driver.sessionID, {
-      passed: passed
-    }, done);
-  })
-
-  test.it('2', function() {
+  it('2', function() {
     driver.get('http://google.com');
 
     var searchBox = driver.findElement(webdriver.By.name('q'));
@@ -57,4 +13,5 @@ test.describe('Google Search 2', function() {
       assert.equal(value, 'webdriver');
     });
   });
+  
 });
